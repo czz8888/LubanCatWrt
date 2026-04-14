37469a0fea28d58e3ec13a3ae726d7cdfa3b7ae4
cheche <3066429068@qq.com>
2026-04-14 09:32:43 +0800

    添加原生uboot

---
+++ sunxi-uboot-src/u-boot-2023/arch/riscv/include/asm/gpio.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright 2018 SiFive, Inc.
+ */
+
+#ifdef CONFIG_GPIO_EXTRA_HEADER
+#include <asm/arch/gpio.h>
+#endif
+#include <asm-generic/gpio.h>
